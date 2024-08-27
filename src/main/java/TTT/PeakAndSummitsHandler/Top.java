7e5a73eb9307865e57c8e5c300e97002a5c857b4
Mefjuu94 <Mateusz94orzel@gmail.com>
2024-08-27 21:17:02 +0200

    first commit

---
+++ src/main/java/TTT/PeakAndSummitsHandler/Top.java
@@ -0,0 +1,2 @@
+package TTT.PeakAndSummitsHandler;public class Top {
+}
---
+++ src/main/java/TTT/TogetherToTheTopApplication.java
@@ -0,0 +1,13 @@
+package TTT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogetherToTheTopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TogetherToTheTopApplication.class, args);
+    }
+
+}
